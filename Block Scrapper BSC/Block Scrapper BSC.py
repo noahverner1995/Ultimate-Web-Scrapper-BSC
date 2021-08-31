56ed3e9acf0761c8eadb78a79b1108aee665703e
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2021-08-31 05:23:18 -0500

    Update Block Scrapper BSC.py
    Just added Try & Except blocks to lines 45 and 118 respectively, due to the frequent appearance of blocks containing no transactions at all or have less than 1 page lol. Now the program is assured to run without being interrupted by this type of problem 👍 , it even prints the Block No. that doesn't have transactions right before proceeding to read the next one

--- Block Scrapper BSC/Block Scrapper BSC.py
+++ Block Scrapper BSC/Block Scrapper BSC.py
@@ -25,8 +25,8 @@ driver.implicitly_wait(10)
 #parameters for the scrapping
 #values for the example
 #Declaring a variable for looping over all the blocks
-blocks = np.arange(9875875, 9875878, 1)
-minimun_value_txn = 0.02
+blocks = np.arange(10479311, 10479314, 1)
+minimun_value_txn = 1.95
 to_chain = 'PancakeSwap: Router v2'
 BNB = 'BNB'
 #set a final dataframe which will contain all the desired data from the arange that matches with the parameters set
@@ -38,82 +38,88 @@ for block in blocks:
     #look for general data of the link
     #amount of results and pages for the execution of the for loop, "block" variable is used within the {} 
     url = 'https://bscscan.com/txs?block={}&p=1'.format(block)
-    driver.get(url)
-    pages =  driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_topPageDiv"]/nav/ul/li[3]/span').text
-    pages = int(re.search(r'\d{0,3}$', pages).group())
-    
-    #FOR LOOP
-    #search at each page all the values we are looking for
-    #following conditions like number of the block, minimun value of the txn and the destiny
-    
-    df = pd.DataFrame()
-    df2 = pd.DataFrame()
-    
     print(f'Bloque actual: {block}')
-    print(df)
-    print(df2)
-    
-    
-    #set a sub loop for each row from the table of each page of each block
-    for page in range(1,pages+1):
-    
-        url = 'https://bscscan.com/txs?block={}&p={}'.format(block,page)
-        driver.get(url)
-        txn_found = int(driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_topPageDiv"]/p/span').text.replace('A total of ','').replace(' transactions found','').replace(',',''))
-        txn_found_last=txn_found%50
-    
-        print(f'Página: {page} de {pages}')
+    driver.get(url)
+    #Here we order the scrapper to try finding the total number of pages for a block if such element that contains it exist
+    #if so, the scrapper will proceed to execute the rest of the procedure
+    try:
+        pages =  driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_topPageDiv"]/nav/ul/li[3]/span').text
+        pages = int(re.search(r'\d{0,3}$', pages).group())
         
-        if page < pages:
-            for txn in range(1,50+1):
-                txn_hash = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[2]'.format(txn)).text)
-                block_num = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[4]'.format(txn)).text)
-                to_destiny = driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[9]'.format(txn)).text
-                value_txn = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[10]'.format(txn)).text)
-                if BNB in value_txn:
-                    value_txn = float(value_txn.replace(' BNB','').replace(',',''))
-                    if value_txn >= minimun_value_txn and to_chain in to_destiny:
-                        row_data = [txn_hash, block_num, value_txn]
-                        df_data = pd.DataFrame([row_data], columns = ['Transaction Id', 'Block No', 'BNB Value'])
-                        df = df.append(df_data, ignore_index = True)
-                        print(df)
-                else:
-                    value_txn = 0            
-                    if value_txn >= minimun_value_txn and to_chain in to_destiny:
-                        row_data = [txn_hash, block_num, value_txn]
-                        df_data = pd.DataFrame([row_data], columns = ['Transaction Id', 'Block No', 'BNB Value'])
-                        df = df.append(df_data, ignore_index = True)
-                        print(df)
-                                                                                         
-        else:
-            for txn in range(1,txn_found_last+1):
-                txn_hash = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[2]'.format(txn)).text)
-                block_num = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[4]'.format(txn)).text)
-                to_destiny = driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[9]'.format(txn)).text
-                value_txn = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[10]'.format(txn)).text)
-                if BNB in value_txn:
-                    value_txn = float(value_txn.replace(' BNB','').replace(',',''))
-                    if value_txn >= minimun_value_txn and to_chain in to_destiny:
-                        row_data2 = [txn_hash, block_num, value_txn]
-                        df_data2 = pd.DataFrame([row_data2], columns = ['Transaction Id', 'Block No', 'BNB Value'])
-                        df2  = df2.append(df_data2, ignore_index = True)
-                        print(df2)
-                        df = df.append(df2, ignore_index = True)
-                else:
-                    value_txn = 0                      
-                    if value_txn >= minimun_value_txn and to_chain in to_destiny:
-                        row_data2 = [txn_hash, block_num, value_txn]
-                        df_data2 = pd.DataFrame([row_data2], columns = ['Transaction Id', 'Block No', 'BNB Value'])
-                        df2  = df2.append(df_data2, ignore_index = True)
-                        print(df2)
-                        df = df.append(df2, ignore_index = True)
-                        
-    #delete every single duplicated row in the df for then adding it to the the final dataframe                
-    df.drop_duplicates(subset='Transaction Id', keep='first', inplace= True)
-    df_final = df_final.append(df, ignore_index = True)
+        #FOR LOOP
+        #search at each page all the values we are looking for
+        #following conditions like number of the block, minimun value of the txn and the destiny
+        
+        df = pd.DataFrame()
+        df2 = pd.DataFrame()
+        
+        
+        print(df)
+        print(df2)
+        
+        
+        #set a sub loop for each row from the table of each page of each block
+        for page in range(1,pages+1):
+        
+            url = 'https://bscscan.com/txs?block={}&p={}'.format(block,page)
+            driver.get(url)
+            txn_found = int(driver.find_element_by_xpath('//*[@id="ContentPlaceHolder1_topPageDiv"]/p/span').text.replace('A total of ','').replace(' transactions found','').replace(',',''))
+            txn_found_last=txn_found%50
+            
+            print(f'Página: {page} de {pages}')
+        
+            if page < pages:
+                for txn in range(1,50+1):
+                    txn_hash = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[2]'.format(txn)).text)
+                    block_num = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[4]'.format(txn)).text)
+                    to_destiny = driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[9]'.format(txn)).text
+                    value_txn = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[10]'.format(txn)).text)
+                    if BNB in value_txn:
+                        value_txn = float(value_txn.replace(' BNB','').replace(',',''))
+                        if value_txn >= minimun_value_txn and to_chain in to_destiny:
+                            row_data = [txn_hash, block_num, value_txn]
+                            df_data = pd.DataFrame([row_data], columns = ['Transaction Id', 'Block No', 'BNB Value'])                        
+                            df = df.append(df_data, ignore_index = True)
+                            print(df)
+                    else:
+                        value_txn = 0            
+                        if value_txn >= minimun_value_txn and to_chain in to_destiny:
+                            row_data = [txn_hash, block_num, value_txn]
+                            df_data = pd.DataFrame([row_data], columns = ['Transaction Id', 'Block No', 'BNB Value'])
+                            df = df.append(df_data, ignore_index = True)
+                            print(df)                                                                 
+            else:
+                for txn in range(1,txn_found_last+1):
+                    txn_hash = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[2]'.format(txn)).text)
+                    block_num = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[4]'.format(txn)).text)
+                    to_destiny = driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[9]'.format(txn)).text
+                    value_txn = str(driver.find_element_by_xpath('//*[@id="paywall_mask"]/table/tbody/tr[{}]/td[10]'.format(txn)).text)
+                    if BNB in value_txn:
+                        value_txn = float(value_txn.replace(' BNB','').replace(',',''))
+                        if value_txn >= minimun_value_txn and to_chain in to_destiny:
+                            row_data2 = [txn_hash, block_num, value_txn]
+                            df_data2 = pd.DataFrame([row_data2], columns = ['Transaction Id', 'Block No', 'BNB Value'])
+                            df2  = df2.append(df_data2, ignore_index = True)
+                            print(df2)
+                            df = df.append(df2, ignore_index = True)
+                    else:
+                        value_txn = 0                      
+                        if value_txn >= minimun_value_txn and to_chain in to_destiny:
+                            row_data2 = [txn_hash, block_num, value_txn]
+                            df_data2 = pd.DataFrame([row_data2], columns = ['Transaction Id', 'Block No', 'BNB Value'])
+                            df2  = df2.append(df_data2, ignore_index = True)
+                            print(df2)
+                            df = df.append(df2, ignore_index = True)
+                            
+        #delete every single duplicated row in the df for then adding it to the the final dataframe                
+        df.drop_duplicates(subset='Transaction Id', keep='first', inplace= True)
+        df_final = df_final.append(df, ignore_index = True)
+    #if such element doesn't exist, then the scrapper will print an statement informing it, for then proceeding to repeat the previous process with the next block.
+    except:
+        print(f'El Bloque No. {block} NO CONTIENE UN CARAJO ¯\_₍⸍⸌̣ʷ̣̫⸍̣⸌₎_/¯, ¡SIGUIENTE! ')
 
 #export results to one single csv file
-df_final.to_csv('ULTIMATE_test_block_9875879_to_9875880_point02_BNB_PANCAKESWAPV2_without_duplicates.csv')
+df_final.to_csv('Block_10479311_to_10479313_1point95_BNB_PANCAKESWAPV2_without_duplicates_NOR_EMPTY_BLOCK_10479312.csv')
 print(df_final)
 driver.quit()
 
